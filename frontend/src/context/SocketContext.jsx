da7404f5f5ef6bf4e52c5206fdd790d25e61d480
Ayush Jha <ayushaj001@gmail.com>
2026-06-16 11:29:58 +0530

    fixed server issue

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000", {
+			const socket = io("https://convo-5u7v.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
