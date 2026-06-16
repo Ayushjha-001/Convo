2033c2820bfc4eee7f67c4497231d1e12649fd68
Ayush Jha <ayushaj001@gmail.com>
2026-06-16 11:57:07 +0530

    fixing database connection

--- backend/db/connectToDb.js
+++ backend/db/connectToDb.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 const connectToMongodb= async()=>{
     try{
+        console.log("Mongo_DB_URI exists:", !!process.env.Mongo_DB_URI);
+    console.log("Available env keys:", Object.keys(process.env).filter(k =>
+      k.toLowerCase().includes("mongo")
+    ));
         await mongoose.connect(process.env.Mongo_DB_URI);
         console.log("connected to Mongodb")
     }
