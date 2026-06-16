23a4ba854fbe7c86bf6cc208de7698cb9ca2c458
Ayush Jha <ayushaj001@gmail.com>
2026-06-16 11:59:48 +0530

    fixed mongoose connection

--- backend/db/connectToDb.js
+++ backend/db/connectToDb.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 const connectToMongodb= async()=>{
     try{
-        console.log("Mongo_DB_URI exists:", !!process.env.Mongo_DB_URI);
-    console.log("Available env keys:", Object.keys(process.env).filter(k =>
-      k.toLowerCase().includes("mongo")
-    ));
-        await mongoose.connect(process.env.Mongo_DB_URI);
+        await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("connected to Mongodb")
     }
     catch(error){
